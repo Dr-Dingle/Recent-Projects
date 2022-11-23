6724af45dd26eacb4fb13e67e0916318e252bad7
Dr-Dingle <114313145+Dr-Dingle@users.noreply.github.com>
2022-11-22 23:45:06 -0500

    Add files via upload

---
+++ yyamltest.python.py
@@ -0,0 +1,34 @@
+import sys
+
+def response():
+    print('Thank you have a nice day!')
+
+
+def chart():
+    filename = 'airportdata.txt'
+    mode = 'r'
+    try:
+        fileObj = open(filename, mode)
+        for lines in fileObj:
+            print(lines)
+    except:
+        print("Unable to open file")
+        sys.exit
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    x=input('Hello, would you like to take a look at our available flight options? Press "Y" to view options or press "N" to quit')
+    if x==
+ 
+    
+main()
